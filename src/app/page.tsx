a946e6d7fe87ad0d98e059c42f2fbc416ba8d91d
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-09-29 21:38:14 +0700

    add my picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <PostOwnner
           PostTitle="Nutcha Khampoung 660610751"
           content="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #26120"
-          profilePic="/profileImages/nutcha.png"
+          profilePic="/profileImages/nutcha.jpg"
           likes={0}
         ></PostOwnner>
 
