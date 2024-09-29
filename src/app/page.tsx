ce2f296e3e5ea7edf8480d97ba535606bf6b9ae5
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-09-29 21:30:02 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import styles from "./page.module.css";
 import { comments } from "@/libs/comments";
 import PostOwnner from "@/components/PostOwnner";
 import Comment from "@/components/Comment";
-<<<<<<< HEAD
 import { ReplyProps } from "../libs/types";
-=======
-import { comments } from "@/libs/comments";
-import { CommentProps } from "@/libs/types";
-import Reply from "@/components/Reply";
->>>>>>> 7c5a7c9a84f60637813f72676faa5978f710e34a
 
 interface CommentComponent {
   userImagePath: string;
@@ -34,12 +28,11 @@ export default function Home() {
         <PostOwnner
           PostTitle="Nutcha Khampoung 660610751"
           content="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #26120"
-          profilePic="/profileImages/nutcha.jpg"
+          profilePic="/profileImages/sajui_pic3.jpg"
           likes={0}
         ></PostOwnner>
 
         {/* map-loop render Comment component here */}
-<<<<<<< HEAD
         {comments.map((cmd: CommentComponent) => (
           <Comment
             key={cmd.username}
@@ -50,24 +43,6 @@ export default function Home() {
             replies={cmd.replies}
           />
         ))}
-=======
-        {/*{comments.map((comment: CommentProps) => (
-        <Comment key={comment.id} id={comment.id} text={comment.text} />
-        ))}*/}
-
-<div>
-  {comments.map((comment, index) => (
-    <Comment
-      key={index}
-      ImagePath={comment.userImagePath}  // ใช้ 'userImagePath' จากข้อมูลของคุณ
-      username={comment.username}        // ใช้ 'username' จากข้อมูลของคุณ
-      commentTitle={comment.commentText} // ใช้ 'commentText' สำหรับเนื้อหาความคิดเห็น
-      likes={comment.likeNum}            // ใช้ 'likeNum' สำหรับจำนวนไลค์
-      replies={comment.replies}          // ใช้ 'replies' สำหรับการตอบกลับ
-    />
-  ))}
-</div>        
->>>>>>> 7c5a7c9a84f60637813f72676faa5978f710e34a
       </div>
     </div>
   );
